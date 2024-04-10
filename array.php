4af35f4f97d75994fd5b5563e4b339c0bd2fd281
pinchan1213 <hello.tra.fujino@gmail.com>
2024-04-10 17:57:52 +0900

    2024/04/10 配列

---
+++ array.php
@@ -0,0 +1,21 @@
+<?php
+$br = '<br>';
+
+$arr1 = [10,20,30];
+$arr2 = array("one"=>1,"two"=>2,"three"=>3);
+
+var_dump($arr1[0]);
+echo $br;
+var_dump($arr2["one"]);
+echo $br;
+
+/*-------------------------------------
+配列 foreachループ
+-----------------------------------*/
+$arr =[10,20,30,40,50];
+foreach($arr as $val){
+  echo $val.$br;
+  // 改行
+  // echo "$val\n";
+}
+?>
