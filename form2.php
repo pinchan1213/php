d35b275744008b499ced26a2b6adda27a82bd684
pinchan1213 <hello.tra.fujino@gmail.com>
2024-04-22 18:05:21 +0900

    2024/04/22 form

---
+++ form2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<h1>Email and URL Validation</h1>
+<form action="hello.php" method="POST">
+   <p><label for="email">Enter your email:</label>
+   <input type="text" id="email" name="email"></p>
+   <p><label for="URL">Enter your website<label>
+   <input type = "text" id="URL" name="url"></p>
+   <input type="submit">
+</form>
+</body>
+</html>
