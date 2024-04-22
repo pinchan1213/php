d69ab4b44cd5ecc78dd4e65c8d18e5163241c082
pinchan1213 <hello.tra.fujino@gmail.com>
2024-04-22 11:36:05 +0900

    2024/04/22 匿名クラス

--- for.php
+++ for.php
@@ -22,4 +22,57 @@ $obj = new myclass();
 foreach($obj as $key){
   print $key;
 }
+
+/*--------------------------------------------------------------------
+オーバーロード
+---------------------------------------------------------------------*/
+class mclass {
+  private $properties = [];
+
+  public function __set($name, $value)
+  {
+    // プロパティを$properties配列に保存
+    $this->properties[$name] = $value;
+    echo $name.$value;
+  }
+
+  public function __get($name){
+    if (array_key_exists($name, $this->properties)) {
+      // $properties配列からプロパティの値を取得
+      return $this->properties[$name];
+    } else {
+      // プロパティが存在しない場合には、特定の処理を行う
+      echo "プロパティ {$name} は存在しません。";
+      return null;
+    }
+  }
+}
+
+$obj = new mclass();
+$obj->myproperty = "こまち";
+echo $obj->myproperty;
+
+
+/*--------------------------------------------------------------------
+匿名クラス
+------------------------------------------------------------------*/
+$obj = new class(10){
+  private $x;
+
+  function __construct($x){
+    $this->x = $x;
+  }
+
+  public function addtion($x){
+    return $this->x+$x;
+  }
+
+  public function div($x){
+    return $this ->x/$x;
+  }
+};
+
+echo $obj  ->addtion(20).$br;
+echo $obj ->div(20).$br;
+
 ?>
